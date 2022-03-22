a96ad39d2974a1968ffd1fe834b5be7eb913d949
gustavosgithub <101032048+gustavosgithub@users.noreply.github.com>
2022-03-22 11:36:20 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,31 +1,18 @@
-onload = function(){
-    let list = localStorage.getItem("tarefas") 
-    document.getElementById("resultado").innerHTML=list
-    document.getElementById("tarefas").value=""
-}
-
-
 var array = []
 
-function elementId(){
-    var timestamp = new Date();
-
-        var id = timestamp.getHours().toString() +
-        timestamp.getMinutes().toString() +
-        timestamp.getSeconds().toString() +
-        timestamp.getMilliseconds().toString(); 
-        return id
+onload = function () {
+    array = JSON.parse(localStorage.getItem("tarefas")) || []
+    updateScreen()
 }
 
 function calcular(){
-    var tarefas = document.getElementById("tarefas").value
-
+    var task = document.getElementById("tarefas").value
+    let data = Date.now()
     var object = {
-        data: elementId(),
-        object2 : {
-            task: tarefas
+        data,
+        task
         }
-    }
+    
     array.push(object)
     updateScreen();
 }
@@ -33,17 +20,17 @@ function calcular(){
 function updateScreen(){
     var list = "<ul>"
     array.forEach((object => {
-        list += "<li  data-object2" + object.data+">"+ object.object2.task+ "</li>"
-        list += "<button class=but1 onclick=deleteTask(this) data-object2="+object.data +">X</button>"
+        list += "<li  data= " + object.data +">"+ object.task+ "</li>"
+        list += "<button class=but1 onclick=deleteTask(this) data="+ object.data +">X</button>"
     }));
     "</ul>"
     document.getElementById("resultado").innerHTML=list
     document.getElementById("tarefas").value=""
 
-    localStorage.setItem("tarefas", list)
+    localStorage.setItem("tarefas", JSON.stringify(array))
 }
 
 function deleteTask(element){
-    array = array.filter(object=>object.data != element.getAttribute("data-object2"))
+    array = array.filter(object=>object.data != element.getAttribute("data"))
     updateScreen()
 }
